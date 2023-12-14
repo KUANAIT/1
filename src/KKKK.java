04331b2e5960d33e19b6be114ca84e48444d03df
KUANAIT <230703@astanait.edu.kz>
2023-12-14 09:52:02 +0600

    232312

---
+++ src/KKKK.java
@@ -0,0 +1,10 @@
+public class KKKK {
+    public static void main(String[] args) {
+        int n = 11;
+        long f = 1;
+        for(int i = 1; i <= n; ++i) {
+            f *= i;
+        }
+        System.out.println(f);
+    }
+}
